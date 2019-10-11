2234501c86f8c4f018242d9ddb699b0b49033361
laxit-patel <laxxitpatel@gmail.com>
2019-10-11 16:20:15 +0530

    God Speed

---
+++ VirtualEnvironment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/laxit/Websites/Python/Django/DailyBazar/VirtualEnvironment/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
